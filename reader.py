90118c7882767de368ccff5bd15128def621664e
Misha <kukubik.02@gmail.com>
2022-08-26 14:00:34 -0700

    rewritten reader.py with pyqtgraph

--- reader.py
+++ reader.py
@@ -1,8 +1,30 @@
 # Read logs for today
-
-import pandas as pd
 from datetime import date, timedelta
+import sys
+import json
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore
+"""
+import pandas as pd
 import os
+"""
+
+PATH = '' # Path where scripts are located, example - 'C:/Users/User/.../monitoring-script/'
+
+
+def format_func(x):
+    '''
+    Function changes seconds into hh:mm:ss format for matplotlib
+    '''
+    hours = int(x//3600)
+    minutes = int((x%3600)//60)
+    seconds = int(x%60)
+
+    #return "{:d}:{:02d}".format(hours, minutes)
+    return "{:d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
 
 def date_():
     '''
@@ -11,6 +33,69 @@ def date_():
     return f'{date.today().strftime("%Y%m%d")}'
 
 
+def load_report():
+    '''
+    Tries to load the report from today.
+    '''
+    try:
+        with open(f'{PATH}logs/log_{date_()}.json', 'r') as file:
+            report = json.load(file)
+        return report
+    except:
+        print('There no log file for today')
+        exit()
+
+
+def qt_window():
+    # creating a pyqtgraph plot window
+    window = pg.plot()
+    window.setGeometry(100, 100, 600, 500)
+    title = "App Using data for today"
+    window.setWindowTitle(title)
+
+    report = load_report()
+    yval = list(report.values())
+    yval_copy = yval.copy()
+    ylab = list(map(lambda x: format_func(x), yval_copy))
+    
+    xlab = list(report.keys())
+    xval = list(range(1,len(xlab)+1))
+
+    y_ticks = []
+    for i, item in enumerate(ylab):
+        y_ticks.append( (yval[i], item) )
+    y_ticks = [y_ticks]
+
+    x_ticks=[]
+    for i, item in enumerate(xlab):
+        x_ticks.append( (xval[i], item) )
+    x_ticks = [x_ticks]
+
+    bargraph = pg.BarGraphItem(x=xval, height=yval, width=0.5, brush='g')
+    window.addItem(bargraph)
+    ax = window.getAxis('bottom')
+    ax.setTicks(x_ticks)
+
+    ay = window.getAxis('left')
+    ay.setTicks(y_ticks)
+    window.showGrid(y=True)
+
+
+
+def main():
+    qt_window()
+    
+    # Start Qt event loop unless running in interactive mode or using
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QApplication.instance().exec_()
+
+
+# main method
+if __name__ == '__main__':
+    main()
+
+
+"""
 LOGFILE = os.getcwd().replace('\\', '/') + '/logs/log_' +date_()+'.json'
 
 
@@ -37,12 +122,16 @@ def read(logfile=LOGFILE):
 
 
 def main():
+
+    '''
     print(LOGFILE)
     print(type(LOGFILE))
     data = read()
     show_log(data)
     input('Press ENTER to quit')
+    '''
 
 
 if __name__=="__main__":
     main()
+"""
